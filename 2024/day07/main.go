85abbe10cdca63b8b2531e36394d6a940bf2964d
MueR <bas@muer.nl>
2024-12-07 14:17:19 +0100

    Day 7

---
+++ 2024/day07/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/MueR/adventofcode.go/cast"
+	"github.com/MueR/adventofcode.go/util"
+)
+
+var (
+	//go:embed input.txt
+	input  string
+	parsed []calibration
+	add    = func(a, b int) int { return a + b }
+	mul    = func(a, b int) int { return a * b }
+	cat    = func(a, b int) int { return cast.ToInt(fmt.Sprintf("%v%v", a, b)) }
+)
+
+type calibration struct {
+	result int
+	input  []int
+}
+
+func (c calibration) Solve(ops []func(a, b int) int) int {
+	if res := FindFormula(c, c.input[0], 1, ops); res != 0 {
+		return res
+	}
+	return 0
+}
+
+func init() {
+	// do this in init (not main) so test file has same input
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 0, "part 1 or 2")
+	flag.Parse()
+
+	s := time.Now()
+	parsed = parseInput(input)
+	fmt.Printf("Parsed input in %v\n", time.Since(s))
+	s = time.Now()
+	if part != 2 {
+		ans := part1()
+		fmt.Printf("Part 1 output: %v  (%v)\n", ans, time.Since(s))
+	}
+	s = time.Now()
+	if part != 1 {
+		ans := part2()
+		fmt.Printf("Part 2 output: %v  (%v)\n", ans, time.Since(s))
+	}
+}
+
+func part1() (res int) {
+	for _, c := range parsed {
+		res += c.Solve([]func(a, b int) int{add, mul})
+	}
+	return res
+}
+
+func part2() (res int) {
+	for _, c := range parsed {
+		res += c.Solve([]func(a, b int) int{add, mul, cat})
+	}
+	return res
+}
+
+func parseInput(input string) (ans []calibration) {
+	for _, line := range strings.Split(strings.Trim(input, "\n"), "\n") {
+		res, inputs, _ := strings.Cut(line, ":")
+		ans = append(ans, calibration{
+			result: cast.ToInt(res),
+			input:  util.LineToInts(inputs),
+		})
+	}
+	return ans
+}
+
+func FindFormula(c calibration, partial, index int, ops []func(a, b int) int) int {
+	if partial > c.result {
+		return 0
+	}
+	if index == len(c.input) {
+		if partial == c.result {
+			return c.result
+		}
+		return 0
+	}
+	for _, op := range ops {
+		np := op(partial, c.input[index])
+		if res := FindFormula(c, np, index+1, ops); res != 0 {
+			return res
+		}
+	}
+	return 0
+}
---
+++ 2024/day07/main_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"testing"
+)
+
+var example = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+`
+
+func TestPart1(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "Example",
+			input: example,
+			want:  3749,
+		},
+		// {
+		// 	name:  "Actual",
+		// 	input: input,
+		// 	want:  12940396350192,
+		// },
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			parsed = parseInput(tt.input)
+			if got := part1(); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "Example",
+			input: example,
+			want:  11387,
+		},
+		{
+			name:  "Actual",
+			input: input,
+			want:  106016735664498,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			parsed = parseInput(tt.input)
+			if got := part2(); got != tt.want {
+				t.Errorf("part2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
